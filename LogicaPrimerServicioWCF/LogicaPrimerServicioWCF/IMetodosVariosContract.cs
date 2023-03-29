f88c235ebc5db395cbae7faebb53caee8d9ae474
Álvaro Gutiérrez Carnicero <alvarogutierrezc6@gmail.com>
2023-03-29 11:50:48 +0300

    C

---
+++ LogicaPrimerServicioWCF/LogicaPrimerServicioWCF/IMetodosVariosContract.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaPrimerServicioWCF
+{
+    [ServiceContract]
+    public interface IMetodosVariosContract
+    {
+        [OperationContract]
+        int GetNumeroDoble(int numero);
+        [OperationContract]
+        string GetSaludo(string nombre);
+        string MetodoInvisible();
+    }
+}
---
+++ LogicaPrimerServicioWCF/LogicaPrimerServicioWCF/MetodosVariosClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaPrimerServicioWCF
+{
+    public class MetodosVariosClass : IMetodosVariosContract
+    {
+        public int GetNumeroDoble(int numero)
+        {
+            return numero * 2;
+        }
+
+        public string GetSaludo(string nombre)
+        {
+            return "Mi primer super Servicio!!!, " + nombre;
+        }
+
+        public string MetodoInvisible()
+        {
+            return "No sirvo para nada, soy invisible";
+        }
+    }
+}
